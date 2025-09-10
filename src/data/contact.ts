5b3f8c0bfa30a13ca3b4d49692567aadc3642dfb
Daviline <156429877+daviline14@users.noreply.github.com>
2025-09-10 13:07:55 +0200

    feat: Refactor contact information handling and update NavBar links for improved navigation

---
+++ src/data/contact.ts
@@ -0,0 +1,12 @@
+// Contact and Social Media Information
+export const contactData = {
+  email: 'davto.xyz@gmail.com',
+  linkedin: 'https://www.linkedin.com/in/david-torres-l%C3%B3pez/',
+  github: 'https://github.com/davto-xyz',
+  twitter: 'https://x.com/davtoxyz',
+  instagram: 'https://www.instagram.com/davto.xyz/',
+};
+
+
+
+export default contactData;
